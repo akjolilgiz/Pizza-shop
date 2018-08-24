9bbc503ec847c81cb712ca0497baf6a4b02e5ece
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-08-24 13:17:15 -0700

    Finish calculations for sizes

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,40 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+var price = 10
+
+ Pizza.prototype.sizeCalculation = function(){
+  if (this.size === "Small") {
+    return price+=0
+  } else if (this.size === "Medium") {
+      return price+=2
+    } else if (this.size === "Large") {
+        return price+=4
+      } else {price += 6}
+}
+
+
+
 
 $(document).ready(function() {
   $("form#orderPizza").submit(function(event) {
     event.preventDefault();
 
-    var name = $("input#client-name").val();
-    var size = $("#sizePicked").val();
-    var toppings = [];
+    var Name = $("input#client-name").val();
+    var Size = $("#sizePicked").val();
+    var Toppings = [];
 
- $("input:checkbox[name=toppingsPicked]:checked").each(function(){
-  toppings.push($(this).val());
+ $("input:checkbox[name=toppingsPicked]:checked").each(function(){ Toppings.push($(this).val());
   });
-    var newPizza = new Pizza (size, toppings);
-    alert(newPizza.toppings);
+    var newPizza = new Pizza (Size, Toppings);
 
 
-    $("#name").text("name: " + name);
-    $("#size").text("size: " + size);
 
-    $(".output").append("<li>" + newPizza.toppings + "</li>");
+    $("#name").text("name: " + Name);
+    $("#size").text("size: " + Size);
+    $("#toppings").text("toppings: " + Toppings)
 
+    $("#price").text("Your total is: " + newPizza.sizeCalculation() + "$");
 
 
 
