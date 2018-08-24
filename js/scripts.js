f8376cc38af628ec065339d637bbcb90e5a94655
Akjol Jaenbai <akjolilgiz@gmail.com>
2018-08-24 15:29:42 -0700

    add picture

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,35 @@
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  this.price = 10;
 }
-// var price = 10
+
 
  Pizza.prototype.sizeCalculation = function() {
   if (this.size === "Small") {
-    return this.price+=0;
+    return this.price=12;
   } else if (this.size === "Medium") {
-    return this.price+=2;
+    return this.price=14;
   } else if (this.size === "Large") {
-    return this.price+=4;
-  } else {price += 6}
+    return this.price=16;
+  } else if (this.size === "ExtraLarge") {
+    return this.price=18}
 }
-  Pizza.prototype.toppingCalculation = function() {
-    if (this.toppings.length === 1){
+  Pizza.prototype.totalPrice = function() {
+    if (this.toppings.length === 0) {
+      return this.price+=0
+    } else if (this.toppings.length === 1){
       return this.price +=2;
-    }else if (this.toppings.length === 2) {
-        return this.price+=4;
-      } else if (this.toppings.length === 3) {
-            return this.price+=6;
+      }else if (this.toppings.length === 2) {
+      return this.price+=4;
+        } else if (this.toppings.length === 3) {
+      return this.price+=6;
           } else if (this.toppings.length === 4) {
-              return this.price+=6;
+      return this.price+=8;
             } else if (this.toppings.length === 5) {
-                return this.price+=6;
+      return this.price+=10;
     }
 }
+
 
 
 $(document).ready(function() {
@@ -40,15 +43,14 @@ $(document).ready(function() {
    $("input:checkbox[name=toppingsPicked]:checked").each(function(){ Toppings.push($(this).val());
     });
     var newPizza = new Pizza (Size, Toppings);
+    $(".receipt").show();
+    $("#name").text(Name);
+    $("#size").text(Size);
+    $("#toppings").text(Toppings)
 
+    $("#price").text(newPizza.sizeCalculation() + "$");
+    $("#total").text(newPizza.totalPrice() + "$");
 
-
-    $("#name").text("name: " + Name);
-    $("#size").text("size: " + Size);
-    $("#toppings").text("toppings: " + Toppings)
-
-    $("#price").text("Your total is: " + newPizza.sizeCalculation() + "$");
-    $("#topPrices").text("Your total is: " + newPizza.toppingCalculation() + "$");
 
 
 });
